4db9af7e17f08fa61fa1451b6112542b6ae9d960
Pantakarn Kowdokrak <peachoenixz@gmail.com>
2022-12-02 03:28:38 +0700

    refactor

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -12,6 +12,12 @@ func TestFizzBuzz(t *testing.T) {
 
 	 tf := []testFizz{
 		 {name : "should 1 get 1",input: 1,want : "1"},
+		 {name : "should 2 get 2",input: 2,want : "2"},
+		 {name : "should 3 get Fizz",input: 3,want : "Fizz"},
+		 {name : "should 4 get 4",input: 4,want : "4"},
+		 {name : "should 5 get Buzz",input: 5,want : "Buzz"},
+		 {name : "should 6 get Fizz",input: 6,want : "Fizz"},
+
 	 }
 
 	for _ , v := range tf{
